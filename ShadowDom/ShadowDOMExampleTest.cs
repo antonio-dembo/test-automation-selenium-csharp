0ba55f0bd490199815d721ea3455de0ce3c9678d
beboantonio <beboantonio1@hotmail.com>
2021-08-22 19:44:45 -0300

    remove unecessary usings

--- ShadowDom/ShadowDOMExampleTest.cs
+++ ShadowDom/ShadowDOMExampleTest.cs
@@ -2,8 +2,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.IO;
-using System.Threading;
 
 namespace ShadowDom
 {
