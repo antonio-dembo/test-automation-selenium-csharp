83b8492261cf0f63cbe12cbc174a585c4943643e
beboantonio <beboantonio1@hotmail.com>
2021-08-22 14:43:28 -0300

    create MSTest project and install selenium packages

---
+++ ShadowDom/ShadowDOMExampleTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShadowDom
+{
+    [TestClass]
+    public class ShadowDOMExampleTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
